212e4c67adea1ab171f70d9ed8f13d944874d518
dkorkmaz <66623897+dkorkmaz@users.noreply.github.com>
2020-06-09 10:56:41 +0300

    Add files via upload

---
+++ LeetCode_42_trapping_rain_water.js
@@ -0,0 +1,27 @@
+let trap = function(height) {
+    if (height === null) {
+        return 0;
+    }
+    
+    let totalWater = 0;
+    let length = height.length;
+    let leftMax = [];
+    let rightMax = [];
+    leftMax[0] = height[0];
+    
+    for (let i = 1; i < length; i++) {
+        leftMax[i] = Math.max(height[i], leftMax[i -  1]);
+    }
+    
+    rightMax[length - 1] = height[length - 1];
+    
+    for (let i = length - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(height[i], rightMax[i + 1])
+    }
+    
+    for (let i = 1; i < length - 1; i++) {
+        totalWater += Math.min(leftMax[i], rightMax[i]) - height[i];
+    }
+    
+    return totalWater;
+};
